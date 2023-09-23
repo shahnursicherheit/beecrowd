b05c8010c43d87b6a59d3e83d40c170c999c2aa3
Md Shahnur <96790202+shahnursicherheit@users.noreply.github.com>
2023-09-24 03:35:03 +0600

    Add files via upload
     beecrowd | 2164
    Fast Fibonacci
    
    By M.C. Pinto, UNILA BR Brazil
    Timelimit: 1
    
    Binet's formula is a way to calculate Fibonacci numbers.
    
    Your task is, given a natural number n, to compute the value of Fibonacci(n) using the formula above.
    Input
    
    The input is a natural number n (0 < n ≤ 50).
    Output
    
    The output is the value of Fibonacci(n) with 1 decimal place using the given Binet's formula.
    Input Samples 	Output Samples
    
    1
    	
    
    1.0
    
    2
    	
    
    1.0
    
    3
    	
    
    2.0

---
+++ Fast Fibonacci.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+    
+    int n;
+    cin>>n;
+    
+    cout<<fixed<<setprecision(1)<<((pow(((1+sqrt(5))/2),n)-pow(((1-sqrt(5))/2),n))/sqrt(5))<<endl;
+    return 0;
+}
